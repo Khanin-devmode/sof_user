fe76b091d434547b2c344569abf3013e83ba8f93
Khanin <khanin.dev@gmail.com>
2023-03-23 22:20:54 +0700

    user rep histoty ui draft

--- lib/features/user_rep/presentation/user_rep_page.dart
+++ lib/features/user_rep/presentation/user_rep_page.dart
@@ -1,9 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:sof_user/features/user_rep/data/user_rep_model.dart';
 import 'package:sof_user/features/user_rep/domain/user_rep_state.dart';
 import 'package:sof_user/features/users_list/data/user_model.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class UserRepPage extends ConsumerStatefulWidget {
   const UserRepPage({super.key, required this.user});
@@ -28,25 +30,26 @@ class UserRepState extends ConsumerState<UserRepPage> {
         .read(userRepNotifierProvider.notifier)
         .getUserRepHistory(user.userId, 1, () {});
 
-    userRepScrollCtrl.addListener(() {
-      int pageToLoad = ref.read(repPageToLoadNumber);
-
-      if ((userRepScrollCtrl.position.pixels ==
-              userRepScrollCtrl.position.maxScrollExtent) &&
-          !isLoading) {
-        setState(() {
-          isLoading = true;
-        });
-        ref
-            .read(userRepNotifierProvider.notifier)
-            .getUserRepHistory(user.userId, pageToLoad, () {
-          ref.read(repPageToLoadNumber.notifier).update((state) => state + 1);
-          setState(() {
-            isLoading = false;
-          });
-        });
-      } else {}
-    });
+    userRepScrollCtrl.addListener(
+      () {
+        int pageToLoad = ref.read(repPageToLoadNumber);
+        var currentPosition = userRepScrollCtrl.position.pixels;
+        var maxPosition = userRepScrollCtrl.position.maxScrollExtent;
+        if ((currentPosition == maxPosition) && !isLoading) {
+          setState(() => isLoading = true);
+          ref.read(userRepNotifierProvider.notifier).getUserRepHistory(
+            user.userId,
+            pageToLoad,
+            () {
+              ref
+                  .read(repPageToLoadNumber.notifier)
+                  .update((state) => state + 1);
+              setState(() => isLoading = false);
+            },
+          );
+        }
+      },
+    );
 
     super.initState();
   }
@@ -55,17 +58,19 @@ class UserRepState extends ConsumerState<UserRepPage> {
   Widget build(BuildContext context) {
     final userRepHistory = ref.watch(userRepNotifierProvider);
 
-    return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: const Text('Reputation History'),
-        ),
-        body: UserRepColumnWidget(
-            user: user,
-            userRepScrollCtrl: userRepScrollCtrl,
-            userRepHistory: userRepHistory,
-            ref: ref,
-            isLoading: isLoading));
+    return LoaderOverlay(
+      child: Scaffold(
+          backgroundColor: Colors.grey[200],
+          appBar: AppBar(
+            title: Text(user.displayName),
+          ),
+          body: UserRepColumnWidget(
+              user: user,
+              userRepScrollCtrl: userRepScrollCtrl,
+              userRepHistory: userRepHistory,
+              ref: ref,
+              isLoading: isLoading)),
+    );
   }
 }
 
@@ -110,16 +115,28 @@ class UserRepColumnWidget extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
+              Row(
+                children: const [
+                  Text('Repuation History'),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               if (userRepHistory.isEmpty) const CircularProgressIndicator()
             ],
           );
         }
         if (index != userRepHistory.length) {
           UserRep rep = userRepHistory[index];
-          return Card(
-            child: ListTile(
-              title: Text(rep.repType),
-            ),
+          return ListTile(
+            dense: true,
+            horizontalTitleGap: 0,
+            leading: getVotedIcon(rep.repType),
+            tileColor: Colors.white,
+            title: Text(getVotedText(rep.repType)),
+            subtitle: Text('${rep.repChange.toString()}'),
+            trailing: Text(getDateTime(rep.dateCreated)),
           );
         } else {
           return Container(
@@ -135,5 +152,53 @@ class UserRepColumnWidget extends StatelessWidget {
         }
       },
     );
+  }
+
+  Icon getVotedIcon(String voteType) {
+    switch (voteType) {
+      case 'post_upvoted':
+        return const Icon(
+          Icons.arrow_upward,
+          color: Colors.green,
+        );
+      case 'post_unupvoted':
+        return const Icon(
+          Icons.arrow_downward,
+          color: Colors.red,
+        );
+      case 'user_deleted':
+        return const Icon(Icons.close);
+      case 'answer_accepted':
+        return const Icon(
+          Icons.check,
+          color: Colors.green,
+        );
+
+      default:
+        return const Icon(Icons.remove);
+    }
+  }
+
+  String getVotedText(String voteType) {
+    switch (voteType) {
+      case 'post_upvoted':
+        return 'Up voted';
+      case 'post_unupvoted':
+        return 'Down voted';
+      case 'user_deleted':
+        return 'Deleted';
+      case 'answer_accepted':
+        return 'Accepted';
+
+      default:
+        return 'Vote';
+    }
+  }
+
+  String getDateTime(int datetime) {
+    var votedTime =
+        DateTime.fromMillisecondsSinceEpoch(datetime * 1000).toLocal();
+
+    return DateFormat('MMM d, yyyy').format(votedTime).toString();
   }
 }
